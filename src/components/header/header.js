7288660d47c2b7e60ea29e6409f74aec105b4882
akash18baaz <akashsharma@Akashs-MacBook-Air.local>
2022-09-05 21:42:09 +0530

    feat - created header

---
+++ src/components/header/header.js
@@ -0,0 +1,20 @@
+import "./header.css"
+import logo from "../../assets/svgs/white.svg"
+
+function Header(){
+    return(
+        <header className="header">
+            <nav className="nav__container">
+                <div className="nav__left-container">
+                    <img src={logo} alt="logo"/>
+                </div>
+                <div className="nav__right-container">
+                    <span className="sign__in-link">Sign In</span>
+                    <button className="primary__btn">Dukaan for PC</button>
+                </div>
+            </nav>
+        </header>
+    )
+}
+
+export {Header}
